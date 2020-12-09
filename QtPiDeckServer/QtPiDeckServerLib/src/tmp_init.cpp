fdb45898e6d02a8ced4ffe351e522f1f71ed18ee
Igor Sharapov <igorsharapov@outlook.com>
2020-12-09 12:03:35 +0300

    Add static lib for main code

---
+++ QtPiDeckServer/QtPiDeckServerLib/src/tmp_init.cpp
@@ -0,0 +1,21 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int tmp_init(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.addImportPath("qrc:/qml/components");
+    const QUrl url(QStringLiteral("qrc:/qml/pages/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
