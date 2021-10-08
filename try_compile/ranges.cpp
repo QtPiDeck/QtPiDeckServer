5db942941ba12aceb2bdb824da6efb079839a90b
Igor Sharapov <igorsharapov@outlook.com>
2021-10-08 21:22:07 +0300

    Add additional checks for ranges

--- try_compile/ranges.cpp
+++ try_compile/ranges.cpp
@@ -1,3 +1,10 @@
 #include <ranges>
+#include <algorithm>
+#include <array>
 
-int main() { return 0; }
+int main() { 
+  const std::array<int, 1> in{0};
+  std::array<int, 1> out{0};
+  std::ranges::transform(in, std::begin(out), [](int val) { return val; });
+  return 0; 
+}
