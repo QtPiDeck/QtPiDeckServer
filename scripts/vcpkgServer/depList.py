953bc409bd2da4635795e97a08078b86a2be6229
Igor Sharapov <igorsharapov@outlook.com>
2021-10-08 23:02:15 +0300

    Fix --head vcpkg argument

--- scripts/vcpkgServer/depList.py
+++ scripts/vcpkgServer/depList.py
@@ -3,5 +3,5 @@ from itertools import chain
 #will be called from server/client script
 def getServerList(args):
     base = ["boost-hana"]
-    add = {"macos-latest": ["nanorange"]}
+    add = {"macos-latest": ["nanorange", "--head"]}
     return chain(base, *[add[arg] for arg in filter(lambda x: x in add, args)])
