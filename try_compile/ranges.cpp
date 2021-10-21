c4c3732b2425fc6b6be2c2c244915b53f6352572
Igor Sharapov <igorsharapov@outlook.com>
2021-10-21 16:31:44 +0300

    Change range test

--- try_compile/ranges.cpp
+++ try_compile/ranges.cpp
@@ -1,3 +1,10 @@
+#include <algorithm>
+#include <array>
 #include <ranges>
 
-int main() { return 0; }
+int main() {
+  const std::array<int, 1> in{0};
+  std::array<int, 1> out{0};
+  std::ranges::transform(in, std::begin(out), [](int val) { return val; });
+  return 0;
+}
