0eb755bc253ef3875a1687acc128a3ef56d8fabf
Igor Sharapov <igorsharapov@outlook.com>
2021-10-31 19:21:08 +0300

    Add tests for DeckServer::start

--- Tests/QtPiDeckServerCodeTests/src/Network/DeckServerTests.cpp
+++ Tests/QtPiDeckServerCodeTests/src/Network/DeckServerTests.cpp
@@ -13,10 +13,10 @@ auto main(int argc, char* argv[]) -> int {
 CT_BOOST_AUTO_TEST_SUITE(DeckServer)
 
 using QtPiDeck::Bus::Message;
+using QtPiDeck::Network::DeckMessages;
 using QtPiDeck::Network::detail::DeckServerPrivate;
 using QtPiDeck::Services::IMessageBus;
 using QtPiDeck::Utilities::Connection;
-using QtPiDeck::Network::DeckMessages;
 
 namespace {
 namespace common {
@@ -67,8 +67,8 @@ public:
 private:
   std::vector<uint64_t> m_subscribedTypes{};
 };
+namespace common {
 class TcpSocket {};
-namespace success {
 class TcpServer : public QObject {
   Q_OBJECT // NOLINT
 public:
@@ -81,9 +81,18 @@ signals:
 private:
   TcpSocket m_socket;
 };
-class DeckServerImpl : public DeckServerPrivate<DeckServerImpl, TcpServer, TcpSocket> {
-  friend DeckServerPrivate<DeckServerImpl, TcpServer, TcpSocket>;
-  using DeckServerPrivate<DeckServerImpl, TcpServer, TcpSocket>::DeckServerPrivate;
+}
+
+namespace success {
+class TcpServer : public common::TcpServer {
+public:
+  [[nodiscard]] auto listen(QHostAddress::SpecialAddress /*address*/, qint16 /*port*/) -> bool { // NOLINT
+    return true;
+  }
+};
+class DeckServerImpl : public DeckServerPrivate<DeckServerImpl, TcpServer, common::TcpSocket> {
+  friend DeckServerPrivate<DeckServerImpl, TcpServer, common::TcpSocket>;
+  using DeckServerPrivate<DeckServerImpl, TcpServer, common::TcpSocket>::DeckServerPrivate;
 
 public:
   [[nodiscard]] auto getServer() -> TcpServer& { return server(); }
@@ -95,6 +104,34 @@ public:
 CT_BOOST_AUTO_TEST_CASE(startShouldInitServerSuccess) {
   auto messageBus = std::make_shared<start::TrackableMessageBus>();
   auto server = start::success::DeckServerImpl(nullptr, messageBus);
+  server.start();
+  auto& tcpServer = server.getServer();
+  tcpServer.emitNewConnection();
+  CT_BOOST_TEST(server.currentConnection() != nullptr);
+  CT_BOOST_TEST(messageBus->subscribedTypes().at(0) == DeckMessages::PingReceived);
+}
+
+namespace {
+namespace start::failure {
+class TcpServer : public common::TcpServer {
+public:
+  [[nodiscard]] auto listen(QHostAddress::SpecialAddress /*address*/, qint16 /*port*/) -> bool { // NOLINT
+    return false;
+  }
+};
+class DeckServerImpl : public DeckServerPrivate<DeckServerImpl, TcpServer, common::TcpSocket> {
+  friend DeckServerPrivate<DeckServerImpl, TcpServer, common::TcpSocket>;
+  using DeckServerPrivate<DeckServerImpl, TcpServer, common::TcpSocket>::DeckServerPrivate;
+
+public:
+  [[nodiscard]] auto getServer() -> TcpServer& { return server(); }
+};
+}
+}
+
+CT_BOOST_AUTO_TEST_CASE(startShouldInitServerFailure) {
+  auto messageBus = std::make_shared<start::TrackableMessageBus>();
+  auto server = start::failure::DeckServerImpl(nullptr, messageBus);
   server.start();
   auto& tcpServer = server.getServer();
   tcpServer.emitNewConnection();
